7d89ff05dc8852118c993b95ab92db32bfd4fd1d
Hannes is tha cool <hannesranningen@gmail.com>
2023-09-12 06:27:31 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormComponent
   ],
   imports: [
     BrowserModule
